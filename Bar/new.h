ad1ba0a7beb9f347eda76716c18f38685f9b912b
liuxuhui <674685799@qq.com>
2015-02-28 15:29:17 +0800

    添加文件

---
+++ Bar/new.h
@@ -0,0 +1,13 @@
+//
+//  new.h
+//  Bar
+//
+//  Created by 刘旭辉 on 15/2/28.
+//  Copyright (c) 2015年 LXH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface new : NSObject
+
+@end
---
+++ Bar/new.m
@@ -0,0 +1,13 @@
+//
+//  new.m
+//  Bar
+//
+//  Created by 刘旭辉 on 15/2/28.
+//  Copyright (c) 2015年 LXH. All rights reserved.
+//
+
+#import "new.h"
+
+@implementation new
+
+@end
